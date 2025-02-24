7abc6d6b084e045fcc88ffc7107acaa24d476b08
Tu Nombre <tuemail@example.com>
2025-02-23 19:16:04 -0600

    feat(home):funcionalidad

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
 })
 export class HomePage {
   constructor(private navCtrl: NavController) {}
 
-  goToCam() {
-    this.navCtrl.navigateForward('/cam');
-  }
-
   goToCatalog() {
     this.navCtrl.navigateForward('/catalog');
+  }
+
+  goToDetails() {
+    this.navCtrl.navigateForward('/details');
+  }
+
+  logout() {
+    this.navCtrl.navigateRoot('/login');
+  }
+
+  openCamera() {
+    console.log('Abrir cámara...');
+    // Aquí puedes agregar código para acceder a la cámara
   }
 }
